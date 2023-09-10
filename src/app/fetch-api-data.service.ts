2e0b0d9723edacd2ee3aabc494d5caeb84d5fcd1
juli20008 <mia.li.mba@gmail.com>
2023-09-09 22:49:22 -0400

    6.3

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { catchError } from 'rxjs/internal/operators';
+import { catchError } from 'rxjs/operators';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -14,19 +14,30 @@ export class FetchApiDataService {
  // This will provide HttpClient to the entire class, making it available via this.http
   constructor(private http: HttpClient) {
   }
- // Making the api call for the user registration endpoint
+   /**
+   * Making the api call for the user registration endpoint
+   * @param userDetails 
+   * @returns a user that has been registered in the DB
+   * used in user-registration-form component 
+   */
   public userRegistration(userDetails: any): Observable<any> {
     console.log(userDetails);
-    return this.http.post(apiUrl + 'users', userDetails).pipe(
+    return this.http.post(apiUrl + 'signup',userDetails).pipe(
     catchError(this.handleError)
     );
   }
- // Making the api call for the user login endpoint
-  public userLogin(userDetails: any): Observable<any> {
-    return this.http.post(apiUrl + 'login?' + new URLSearchParams(userDetails), {}).pipe(
-      catchError(this.handleError)
-    );
-  }
+  /**
+   * direct users to the login page.
+   * @param userDetails 
+   * @returns will log in the user with a token and user info in local storage
+   * used in user-login-form component 
+   */
+   public userLogin(userDetails: any): Observable<any> {
+  // console.log(userDetails);
+  return this.http.post(apiUrl + 'login', userDetails).pipe(
+    catchError(this.handleError)
+  );
+}
 //Making the api call for the get all movies endpoint
   getAllMovies(): Observable<any> {
     const token = localStorage.getItem('token');
@@ -170,15 +181,19 @@ export class FetchApiDataService {
     return body || {};
   }
 
-private handleError(error: HttpErrorResponse): any {
+  private handleError(error: HttpErrorResponse): any {
     if (error.error instanceof ErrorEvent) {
-    console.error('Some error occurred:', error.error.message);
+      console.log(error);
+      console.log(error.error);
+      console.error('Some error occurred:', error.error.message);
     } else {
-    console.error(
+      console.log(error);
+      console.log(error.error);
+      console.error(
         `Error Status code ${error.status}, ` +
         `Error body is: ${error.error}`);
     }
-    return throwError(
-    'Something bad happened; please try again later.');
+    return throwError(() =>
+      new Error('Something bad happened; please try again later.'));
   }
 }
